60000a8b50a13da1f1c30bc4bcb6c1261bf308a6
mark <markpackham1@gmail.com>
2022-09-13 19:15:23 +0100

    demo options API solution instead of composition API one
    https://youtu.be/YjvcyI40qas?t=297

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
+<script>
 import TheWelcome from "@/components/TheWelcome.vue";
+
+export default {
+  components: { TheWelcome },
+
+  data() {
+    return {
+      message: "Hello World",
+    };
+  },
+
+  mounted() {
+    alert("I have been mounted");
+  },
+};
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
   </main>
 </template>
