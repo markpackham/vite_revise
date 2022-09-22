15aadb207598bc31ba668df6cf6a668ef847cfa7
mark <markpackham1@gmail.com>
2022-09-22 08:37:56 +0100

    get food from local storage
    https://youtu.be/HJAImAlZzxk?t=187

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@ let { flash } = useFlash();
 <script setup>
 import { ref } from "vue";
 
-let food = ref('');
+let food = ref(localStorage.getItem("food"));
 
 function write() {
   localStorage.setItem('food', food.value);
