5dc69900add7846f5504df53ed3c1ad3c2e2d76a
mark <markpackham1@gmail.com>
2022-09-22 08:45:37 +0100

    store both food and age
    https://youtu.be/HJAImAlZzxk?t=246

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,9 +16,11 @@ let { flash } = useFlash();
 import { ref } from "vue";
 
 let food = ref(localStorage.getItem("food"));
+let age = ref(localStorage.getItem("age"));
 
-function write() {
-  localStorage.setItem('food', food.value);
+function write(key, value) {
+  localStorage.setItem(key, value);
+
 }
 </script>
 
@@ -26,7 +28,11 @@ function write() {
   <div>
     <main>
       <p>
-        What is your fav food? <input type="text" v-model="food" @input="write" />
+        What is your fav food? <input type="text" v-model="food" @input="write('food',food)" />
+      </p>
+
+      <p>
+        How old are you <input type="text" v-model="age" @input="write('age',age)" />
       </p>
     </main>
   </div>
