1090068abc392450d74536b4f90cab4412ee22de
mark <markpackham1@gmail.com>
2022-09-22 08:33:18 +0100

    create write function for food
    https://youtu.be/HJAImAlZzxk?t=125

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { useFlash } from "@/composables/useFlash";
 
 let { flash } = useFlash();
@@ -10,4 +10,25 @@ let { flash } = useFlash();
       <button @click="flash('Test', 'It Works', 'success')">Click Me</button>
     </p>
   </main>
+</template> -->
+
+<script setup>
+import { ref } from "vue";
+
+let food = ref('');
+
+function write() {
+  localStorage.setItem('food', food.value);
+}
+</script>
+
+<template>
+  <div>
+    <main>
+      <p>
+        What is your fav food? <input type="text" v-model="food" @input="write" />
+      </p>
+    </main>
+  </div>
 </template>
+
