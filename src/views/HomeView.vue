385271131796743853083cb9d7d66b2a82a2ba0f
mark <markpackham1@gmail.com>
2022-09-27 21:12:51 +0100

    add a v-model for comments
    https://youtu.be/A0V6a8D1hFM?t=660

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <script setup>
+import { ref } from "vue";
 import TabbableTextarea from "../components/TabbableTextarea.vue";
 
 
+
+let comment = ref('Test Value');
 </script>
 
 <template>
   <main>
     <div>
       <form>
-        <TabbableTextarea style="width:100%; height:400px;" />
+        <TabbableTextarea v-model="comment" style="width:100%; height:400px;" />
       </form>
     </div>
   </main>
